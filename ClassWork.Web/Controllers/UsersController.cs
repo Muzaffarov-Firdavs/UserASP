044469a04dcd293de03b0a6ce5d22c5e583cf4a9
Muzaffarov03 <firdavsmuzaffarov03@gmail.com>
2023-04-29 13:56:51 +0500

    added users controller and response

---
+++ ClassWork.Web/Controllers/UsersController.cs
@@ -0,0 +1,53 @@
+﻿using ClassWork.Service.DTOs.Users;
+using ClassWork.Service.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ClassWork.Web.Controllers
+{
+    public class UsersController : BaseController
+    {
+        private readonly IUserService userService;
+        public UsersController(IUserService userService)
+        {
+            this.userService = userService;
+        }
+
+        [HttpPut("update")]
+        public async Task<IActionResult> PutUserAsync(UserForUpdateDto dto)
+            => Ok(new
+            {
+                Code = 200,
+                Error = "Success",
+                Data = await this.userService.UpdateAsync(dto)
+            });
+
+        [HttpDelete("delete/{id:long}")]
+        public async Task<IActionResult> DeleteUserASync(long id)
+            => Ok(new
+            {
+                Code = 200,
+                Error = "Success",
+                Data = await this.userService.DeleteAsync(id)
+            });
+
+        [HttpGet("get-by-id/{id:long}")]
+        public async Task<IActionResult> GetUserAsync(long id)
+            => Ok(new
+            {
+                Code = 200,
+                Error = "Success",
+                Data = await this.userService.RetriewAsync(id)
+            });
+
+        [HttpGet("get-list")]
+        public async Task<IActionResult> GetAllUsersAsync()
+            => Ok(new
+            {
+                Code = 200,
+                Error = "Success",
+                Data = await this.userService.RetriewAllAsync()
+            });
+
+
+    }
+}
---
+++ ClassWork.Web/Models/Response.cs
@@ -0,0 +1,9 @@
+﻿namespace ClassWork.Web.Models
+{
+    public class Response
+    {
+        public int Code { get; set; }
+        public string Error { get; set; }
+        public object Data { get; set; }
+    }
+}
